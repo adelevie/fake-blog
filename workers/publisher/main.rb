6b922a3b1337b93c7c7da1272e6823dd399e8f1c
Alan deLevie <adelevie@gmail.com>
2013-03-09 16:49:56 -0500

    writing files

--- workers/publisher/main.rb
+++ workers/publisher/main.rb
@@ -1,4 +1,5 @@
 require 'cgi'
+require 'erb'
 
 cgi_parsed = CGI::parse(payload)
 parsed = JSON.parse(cgi_parsed['payload'][0])
@@ -13,3 +14,12 @@ clone_url = "git://github.com/#{owner_name}/#{repo_name}.git"
 Dir.chdir(repo_name)
 
 p `ls`
+
+
+open('_jekyll-s3.yml', 'w') { |f|
+  f << "s3_id: YOUR_AWS_S3_ACCESS_KEY_ID\n"
+  f << "s3_secret: YOUR_AWS_S3_SECRET_ACCESS_KEY\n"
+  f << "s3_bucket: your.blog.bucket.com\n"
+}
+
+p `cat _jekyll-s3.yml`
