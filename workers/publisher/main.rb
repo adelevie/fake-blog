3910b607bb3dddc788b071aa0bd93145e3aac6b6
Alan deLevie <adelevie@gmail.com>
2013-03-09 16:45:47 -0500

    error prone

--- workers/publisher/main.rb
+++ workers/publisher/main.rb
@@ -4,7 +4,7 @@ cgi_parsed = CGI::parse(payload)
 parsed = JSON.parse(cgi_parsed['payload'][0])
 
 repo_name = parsed["repository"]["name"]
-owner_name = ["repository"]["owner"]["name"]
+owner_name = parsed["repository"]["owner"]["name"]
 
 clone_url = "git://github.com/#{owner_name}/#{repo_name}.git"
 
