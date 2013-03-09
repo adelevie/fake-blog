aeb55f2700677d04a9445cad633b19bc433c1158
Alan deLevie <adelevie@gmail.com>
2013-03-09 16:41:20 -0500

    now were getting serious

--- workers/publisher/main.rb
+++ workers/publisher/main.rb
@@ -1,18 +1,7 @@
 require 'cgi'
 
-`git clone `
-
-puts payload.class
-puts payload
-
-p "cgi parsing:"
-
 cgi_parsed = CGI::parse(payload)
-p cgi_parsed.inspect
-
-p "json parsing"
-
 parsed = JSON.parse(cgi_parsed['payload'][0])
-p "parsed: #{parsed.inspect}"
 
-p parsed["repository"]
+p parsed["repository"]["name"]
+p parsed["repository"]["owner"]["name"]
