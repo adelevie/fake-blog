6625b63b6c4f3ddb0324232e02e682b4314a748a
Alan deLevie <adelevie@gmail.com>
2013-03-09 16:33:25 -0500

    what's old is new

--- workers/publisher/main.rb
+++ workers/publisher/main.rb
@@ -15,3 +15,13 @@ require 'cgi'
 
 puts payload.class
 puts payload
+
+p "cgi parsing:"
+
+cgi_parsed = CGI::parse(payload)
+p cgi_parsed.inspect
+
+p "json parsing"
+
+parsed = JSON.parse(cgi_parsed['payload'][0])
+p "parsed: #{parsed.inspect}"
