36700e35578629077e47ccb1375344139df02022
Alan deLevie <adelevie@gmail.com>
2013-03-09 16:16:05 -0500

    using github hooks to develop software that uses github hooks to build blogs using github

--- workers/publisher/main.rb
+++ workers/publisher/main.rb
@@ -1,7 +1,9 @@
-require 'cgi'
+#require 'cgi'
 
-cgi_parsed = CGI::parse(payload)
-p "cgi_parsed: #{cgi_parsed.inspect}"
+#cgi_parsed = CGI::parse(payload)
+#p "cgi_parsed: #{cgi_parsed.inspect}"
  
-parsed = JSON.parse(cgi_parsed['payload'][0])
-p "parsed: #{parsed.inspect}"
+#parsed = JSON.parse(cgi_parsed['payload'][0])
+#p "parsed: #{parsed.inspect}"
+
+p "#{JSON.parse(payload)}"
