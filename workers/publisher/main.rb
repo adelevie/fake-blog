dc98d3659faa36c7095ffcab890283592f55d044
Alan deLevie <adelevie@gmail.com>
2013-03-09 16:43:09 -0500

    cooking wit gas

--- workers/publisher/main.rb
+++ workers/publisher/main.rb
@@ -3,5 +3,9 @@ require 'cgi'
 cgi_parsed = CGI::parse(payload)
 parsed = JSON.parse(cgi_parsed['payload'][0])
 
-p parsed["repository"]["name"]
-p parsed["repository"]["owner"]["name"]
+repo_name = parsed["repository"]["name"]
+owner_name = ["repository"]["owner"]["name"]
+
+clone_url = "git://github.com/#{owner_name}/#{repo_name}.git"
+
+`git clone #{clone_url}`
